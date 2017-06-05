fed78efb5f213e1a2bfe8ad419acdd2aad578d98
Gabriel Simoes <gabriel.simoes@gmail.com>
2017-06-05 14:06:35 -0300

    Make model convertion compatible with python2.7

--- yad2k.py
+++ yad2k.py
@@ -5,6 +5,7 @@ Reads Darknet19 config and weights and creates Keras model with TF backend.
 Currently only supports layers in Darknet19 config.
 """
 
+from __future__ import print_function
 import argparse
 import configparser
 import io
@@ -57,7 +58,7 @@ def unique_config_sections(config_file):
                 _section = section + '_' + str(section_counters[section])
                 section_counters[section] += 1
                 line = line.replace(section, _section)
-            output_stream.write(line)
+            output_stream.write(unicode(line))
     output_stream.seek(0)
     return output_stream
 
@@ -259,7 +260,7 @@ def _main(args):
     print('Read {} of {} from Darknet weights.'.format(count, count +
                                                        remaining_weights))
     if remaining_weights > 0:
-        print('Warning: {} unused weights'.format(len(remaining_weights)))
+        print('Warning: {} unused weights'.format(len(int(remaining_weights))))
 
     if args.plot_model:
         plot(model, to_file='{}.png'.format(output_root), show_shapes=True)
